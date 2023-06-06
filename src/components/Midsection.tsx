27fe3ded3be3fcfe456ecdd2a71796ae63772f09
Paul Morgan <orbitingmind@gmail.com>
2023-06-06 01:06:37 -0400

    improved section

--- src/components/Midsection.tsx
+++ src/components/Midsection.tsx
@@ -2,6 +2,12 @@ export default  function Midsection(){
 
         return <section className="section-1">
 
+            <div className="info">
+                   <h1>New Toppings</h1>
+            </div>
+
+
+
 
         </section>
 
